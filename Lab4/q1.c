6bd97ce22d73904b51fd8e8e3fd835f8a4c6dea5
ClaytonWas <claywasmund@gmail.com>
2025-02-03 15:37:20 -0500

    lab4 done

--- Lab4/q1.c
+++ Lab4/q1.c
@@ -2,7 +2,11 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Error: %s expects 2 additional arguments. You passed %d \n.", argv[0], argc-1);
+        return 1;
+    }
     //process idenifier type for system calls return ids
     pid_t pid;
     //every time a fork is done pid will change
@@ -15,22 +19,23 @@ int main() {
         return 1; 
     } else if (pid == 0) {
     // fork id of parent is never 0
-        char *args[] = {"ls", NULL};
+        char *cmd[] = {argv[1], NULL};
 
         printf("I am a child process with id: %d \n", getpid());
-        printf("I'll execute: ls \n");
-        printf("Below are the results for: command1 \n \n");
+        printf("I'll execute: %s \n", argv[1]);
+        printf("Below are the results for: %s \n \n", argv[1]);
 
-        execvp(args[0], args);
+        execvp(cmd[0], cmd);
         perror("execvp");
     } else {
-        char *args[] = {"cal", NULL};
+        char *cmd[] = {argv[2], NULL};
+        wait(NULL);
 
-        printf("I am a parent process with id: %d \n", getpid());
-        printf("I'll execute: whoami \n");
-        printf("Below are the results for: command2 \n \n");
+        printf("\nI am a parent process with id: %d \n", getpid());
+        printf("I'll execute: %s \n", argv[2]);
+        printf("Below are the results for: %s \n \n", argv[2]);
 
-        execvp(args[0], args);
+        execvp(cmd[0], cmd);
         perror("execvp");
     }
 
