2500ac351bfa574ce3c8686593fec604183ec665
ClaytonWas <claywasmund@gmail.com>
2025-02-03 15:12:14 -0500

    lab4 almost done

--- Lab4/q1.c
+++ Lab4/q1.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <sys/wait.h>
 
-int main(int argc) {
+int main() {
     //process idenifier type for system calls return ids
     pid_t pid;
-
     //every time a fork is done pid will change
     pid = fork();
 
-    printf(argc);
-    
+
     // fork should never be negetive
     if (pid < 0) {
         fprintf(stderr, "Fork failed");
         return 1; 
     } else if (pid == 0) {
     // fork id of parent is never 0
+        char *args[] = {"ls", NULL};
+
         printf("I am a child process with id: %d \n", getpid());
-        printf("I'll execute: command1 \n");
-        printf("Below are the results for: command1 \n");
+        printf("I'll execute: ls \n");
+        printf("Below are the results for: command1 \n \n");
+
+        execvp(args[0], args);
+        perror("execvp");
     } else {
+        char *args[] = {"cal", NULL};
+
         printf("I am a parent process with id: %d \n", getpid());
-        printf("I'll execute: command2 \n");
+        printf("I'll execute: whoami \n");
         printf("Below are the results for: command2 \n \n");
+
+        execvp(args[0], args);
+        perror("execvp");
     }
 
 
