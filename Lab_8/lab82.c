64beb139e98a49924b4f73f399368ec3d34a1b7f
dantedanette <carminedante@yahoo.com>
2025-03-17 15:03:19 -0400

    asdsad

--- Lab_8/lab82.c
+++ Lab_8/lab82.c
@@ -15,30 +15,38 @@ int main()
     {
         char name[100];
         int family = address->ifa_addr->sa_family;
-        printf("Family: %d\n", family);
+      //  printf("Family: %d\n", family);
         int size = sizeof(struct sockaddr_in);
         getnameinfo(address->ifa_addr, size, name, sizeof(name), 0, 0, 0);
-        printf("Interface Name: %s\n", name);
+       // printf("Interface Name: %s\n", name);
         address = address->ifa_next; // adress += 1
     }
-    printf("************************************code starts here*********************\n");
+   // printf("************************************code starts here*********************\n");
 
     //we do the same thing as before but pring only if ipv4 or ipv6
     address = addresses;
     while (address != NULL) {
 
-
+            char name[100];
             int family = address->ifa_addr->sa_family;
+            int size ;
 
             // check if ipv4 or 6
             if (family == AF_INET || family == AF_INET6) {
-                
                 if (family == AF_INET){
-                    printf("IPv4 Interface: %s\n", address->ifa_name);
+                    size = sizeof(struct sockaddr_in);
+                }
+                else{
+                    size = sizeof(struct sockaddr_in6);
+                }
+                getnameinfo(address->ifa_addr, size, name, sizeof(name), 0, 0, 0);
+                if (family == AF_INET){
+
+                    printf("IPv4 Interface: %s\n",  name);
                 }
 
                 else if (family == AF_INET6){
-                    printf("IPv6 Interface: %s\n", address->ifa_name);
+                    printf("IPv6 Interface: %s\n",name);
                 }
         }
         address = address->ifa_next;
